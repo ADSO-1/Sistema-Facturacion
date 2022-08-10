046583090c1f2e32d6191929b4aef3d2a4b152fa
MDMS29 <103197915+MDMS29@users.noreply.github.com>
2022-08-10 14:02:18 -0500

    Update Modelo.js

--- SistemaFacturacion/js/Controlador/Modelo.js
+++ SistemaFacturacion/js/Controlador/Modelo.js
@@ -3,8 +3,11 @@ import { cliente } from "../index.js"
 import { sincronizarStorage } from "../helpers/funciones.js"
 
 export const guardarCliente = (tipoDoc, nidentificacion, genero, nombre, apellido, fecha_nac, nacio) => {
-    const persona = new Cliente(tipoDoc, nidentificacion, genero, nombre, apellido, fecha_nac, nacio)
-    cliente.push(persona)
-    persona.mostrarCliente()
-    sincronizarStorage(cliente)
+    const persona = new Cliente(tipoDoc, nidentificacion, genero, nombre, apellido, fecha_nac, nacio) //Guarda un nuevo Cliente
+    cliente.push(persona) //El cliente se guarda como objeto para poder enviarlo al localStorage
+    sincronizarStorage(cliente) //Funcion para sincronizarStorage el localStorage
+}
+
+export const eliminarCliente = () => { //Prueba para le Grupo xddxxdxd
+
 }
