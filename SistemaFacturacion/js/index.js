887fcbe80c1f4738a1d712b975dbed3ae59697f3
MDMS29 <103197915+MDMS29@users.noreply.github.com>
2022-08-10 14:01:35 -0500

    Update index.js

--- SistemaFacturacion/js/index.js
+++ SistemaFacturacion/js/index.js
@@ -1,19 +1,26 @@
-console.log("Conectado...");
 import { guardarCliente } from "./Controlador/Modelo.js";
-export let cliente = []
+import { limpiarHTML, imprimirLS } from "./helpers/funciones.js";
+export let cliente = [] //Array que guarda lo que tiene el localStorage - Set
+export let clienteLS //Array que guarda la informacion en el localStorage - Get
 
 const formulario = document.querySelector("#cliente");
 
 document.addEventListener("DOMContentLoaded", () => {
+    clienteLS = JSON.parse(localStorage.getItem('cliente')) ?? []//Toma los valores del localStorage
+    for(let i = 0 ; i < clienteLS.length; i++) cliente.push(clienteLS[i])//Guarda en el array principal todos los elementos del Local Storage par cuando se actuzalice la pagina
+    imprimirLS()//Muestra en lista todos los clientes del localStorage
     const tituloLista = document.querySelector('.tituloLista')
-    if(cliente.length >= 0){
+    if(clienteLS.length == 0){    
         tituloLista.classList.add('empty')
-        tituloLista.textContent = 'No hay Clientes Registrados'
+        tituloLista.textContent = 'No hay Clientes Registrados'//Si no hay clientes en el LocalStorage tendra este titulo
+    }else{
+        return
     }
-});
+})
 
 formulario.addEventListener("submit", function(e){
-    e.preventDefault();    
+    e.preventDefault();
+    limpiarHTML() //Limpia el HTML para que no se muestra doblemente la informacion
     const tipoDoc = document.querySelector("#tipoDocumento").value
     const nidentificacion = document.querySelector("#doc").value
     const genero = document.querySelector("#genero").value
@@ -21,6 +28,8 @@ formulario.addEventListener("submit", function(e){
     const apellido = document.querySelector("#apellido").value
     const fecha_nac = document.querySelector("#fecha_nac").value
     const nacio = document.querySelector("#nac").value
-    guardarCliente(tipoDoc, nidentificacion, genero, nombre, apellido, fecha_nac, nacio)
-    formulario.reset()
+    guardarCliente(tipoDoc, nidentificacion, genero, nombre, apellido, fecha_nac, nacio)//Manda a guardar la informacion del cliente en una funcion
+    formulario.reset()//Resetea el formulario
+    clienteLS = JSON.parse(localStorage.getItem('cliente')) ?? []//Toma los valores del localStorage
+    imprimirLS()//Muestra en lista todos los clientes del localStorage
 })
